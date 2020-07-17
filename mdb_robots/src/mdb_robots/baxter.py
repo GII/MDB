921d4ffff94204b0eddc61e00da391a031fcc448
José Antonio Becerra Permuy <jose.antonio.becerra.permuy@udc.es>
2020-07-17 11:24:23 +0200

    Playing with vegetals detection and recognition ;-)

--- mdb_robots/src/mdb_robots/baxter.py
+++ mdb_robots/src/mdb_robots/baxter.py
@@ -8,9 +8,8 @@ Distributed under GPLv3.
 from __future__ import absolute_import, division, print_function, unicode_literals
 from builtins import *  # noqa
 import numpy
-import skimage
-import skimage.morphology # it doesn't work in Ubuntu 14.04 without this!!!
-import skimage.feature
+from skimage import color, filters, morphology, feature, draw, io, util, transform
+from scipy import ndimage
 import rospy
 from mdb_robots.robot import Robot
 
@@ -27,34 +26,56 @@ class Baxter(Robot):
         """Randomize the state of the environment."""
         # Wait for the human been...
 
-    @staticmethod
-    def detect_objects(image):
+    @classmethod
+    def detect_objects(cls, image):
         """Try to find colour objects in a white table."""
         # RGB => Gray
-        gray_image = skimage.color.rgb2gray(image)
-        # Binarize using a threshold
-        binarized_image = (gray_image < skimage.filters.threshold_mean(gray_image)).astype(float)
-        # Dilate the image to remove artifacts and fill gaps
-        binarized_image = skimage.morphology.dilation(binarized_image, selem=skimage.morphology.star(5))
-        # Paint the space outside the table black
-        dim = binarized_image.shape
-        # Be ware! Due to flood_fill, this needs scikit-image 0.15.0
-        #binarized_image = skimage.morphology.flood_fill(binarized_image, seed_point=(0, 0), new_value=0)
-        #binarized_image = skimage.morphology.flood_fill(
-        #    binarized_image, seed_point=(dim[0] - 1, dim[1] - 1), new_value=0
-        #)
+        gray_image = color.rgb2gray(image)
+        # Improve contrast using a local method
+        enhanced_image = filters.rank.enhance_contrast(util.img_as_ubyte(gray_image), selem=morphology.disk(2))
+        # Detect edges using Canny + closing + fill holes (it misses part of the table's edge)
+        edges = feature.canny(enhanced_image, sigma=2.3)
+        closed_edges = ndimage.binary_closing(edges, iterations=5)
+        binarized_image = ndimage.binary_fill_holes(closed_edges)
+        io.imshow(binarized_image)
+        io.show()
+        # Detect edges using gradient + Otsu global thresholding (it misses some small objects)
+        gradient = filters.rank.gradient(enhanced_image, selem=morphology.disk(3))
+        binarized_image_2 = gradient > filters.threshold_otsu(gradient)
+        io.imshow(binarized_image_2)
+        io.show()
+        # Combine both edge images
+        combined_binarized_image = numpy.maximum(binarized_image, binarized_image_2)
+        # Paint the space outside the table black. Due to flood_fill(), this needs scikit-image 0.15.0
+        dim = combined_binarized_image.shape
+        combined_binarized_image = morphology.flood_fill(combined_binarized_image, seed_point=(0, 0), new_value=255)
+        combined_binarized_image = morphology.flood_fill(
+            combined_binarized_image, seed_point=(dim[0] - 1, dim[1] - 1), new_value=255
+        )
+        io.imshow(combined_binarized_image)
+        io.show()
         # Look for blobs. This works better with the gray image than with the binarized image
-        blobs = skimage.feature.blob_doh(gray_image, threshold=0.006)
+        blobs = feature.blob_dog(combined_binarized_image, threshold=1.0, exclude_border=1)
         # Paint red circles around the blobs
-        undesired_blobs = []
-        for idx, blob in enumerate(blobs):
-            row, col, radius = [int(round(x)) for x in blob]
-            if binarized_image[row, col]:
-                rows, cols = skimage.draw.circle_perimeter(row, col, radius, shape=image.shape)
-                image[rows, cols] = (255, 0, 0)
-            else:
-                undesired_blobs.append(idx)
-        return numpy.delete(blobs, undesired_blobs, 0), image
+        for blob in blobs:
+            row, col, sigma = [int(round(x)) for x in blob]
+            rows, cols = draw.circle_perimeter(row, col, 7, shape=image.shape)
+            image[rows, cols] = (255, 0, 0)
+            lbp_code_red = feature.multiblock_lbp(transform.integral_image(image[:, :, 0]), row - 7, col - 7, 5, 5)
+            lbp_code_green = feature.multiblock_lbp(transform.integral_image(image[:, :, 1]), row - 7, col - 7, 5, 5)
+            lbp_code_blue = feature.multiblock_lbp(transform.integral_image(image[:, :, 2]), row - 7, col - 7, 5, 5)
+            print(
+                str(row)
+                + "\t"
+                + str(col)
+                + "\t"
+                + str(lbp_code_red)
+                + "\t"
+                + str(lbp_code_green)
+                + "\t"
+                + str(lbp_code_blue)
+            )
+        return blobs, image
 
     def update_objects(self, blobs):
         """Update detected objects."""
